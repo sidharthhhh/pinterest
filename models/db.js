43be836c314b2eaf37dbd9f6fd352e2bd2d82c05
Sidharth Shukla <swayam1044@gmail.com>
2023-08-10 13:50:17 +0530

    db added to cloud

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterest")
+mongoose.connect("mongodb+srv://pinterest:pinterest@cluster0.yzkb1jo.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log("db connected!!!"))
     .catch((err) => console.log(err));
 
